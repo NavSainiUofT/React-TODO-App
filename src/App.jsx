35b8bcf83d10f547ddca796c4f5cbb3d7b7374bd
NavSainiUofT <thenavsta@gmail.com>
2022-12-12 10:11:49 -0500

    added map function and && ? :

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const title = "Blog Post";
+  const body = "This is my blog post body";
+  const comments = [
+    {id: 1, text:"Comment one"},
+    {id: 2, text:"Comment two"},
+    {id: 3, text:"Comment three"},
+  ]
+  const loading = false;
+  const showComments = true;
+
+  const commentBlock = (<div className="comments">
+  <h3>Comments ({comments.length})</h3>
+  <ul>
+    {comments.map((comment,index)=>(
+        <li key={index}>
+          {comment.text}
+        </li>
+    ))}
+  </ul>
+</div>)
+
+  if(loading) {return <h1>Loading...</h1>}
 
   return (
     <>
-    <h1>MY APP</h1>
+      <h1>{title}</h1>
+      <p>{body}</p>
+
+      {showComments && commentBlock}
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
